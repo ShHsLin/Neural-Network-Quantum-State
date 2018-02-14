Fix error in variance calculation

NQS.py
@@ -252,7 +252,7 @@ class NQS_1d():
             Eavg = np.average(Earray)
             Evar = np.var(Earray)
             print(self.get_self_amp_batch()[:5])
-            print("E/N !!!!: ", Eavg / L, "  Var: ", Evar / L / np.sqrt(num_sample))  # , "Earray[:10]",Earray[:10]
+            print("E/N !!!!: ", Eavg / L, "  Var: ", Evar / (L**2) / num_sample)  # , "Earray[:10]",Earray[:10]
             Glist = self.NNet.vanilla_back_prop(configArray, Earray)
             # Reg
             for idx, W in enumerate(self.NNet.sess.run(self.NNet.para_list)):
@@ -262,7 +262,7 @@ class NQS_1d():
             end_c, end_t = time.clock(), time.time()
             print("monte carlo time ( backProp ): ", end_c - start_c, end_t - start_t)
             print("norm(G): ", np.linalg.norm(Gj))
-            return Gj, Eavg / L, Evar / L / np.sqrt(num_sample)
+            return Gj, Eavg / L, Evar / (L**2) / num_sample
         else:
             pass
 
@@ -298,7 +298,7 @@ class NQS_1d():
         Eavg = np.average(Earray)
         Evar = np.var(Earray)
         print(self.get_self_amp_batch()[:5])
-        print("E/N !!!!: ", Eavg / L, "  Var: ", Evar / L / np.sqrt(num_sample))
+        print("E/N !!!!: ", Eavg / L, "  Var: ", Evar / (L**2) / num_sample)
 
         #####################################
         #  Fj = 2<O_iH>-2<H><O_i>
@@ -353,7 +353,7 @@ class NQS_1d():
         end_c, end_t = time.clock(), time.time()
         print("Sij, Fj time: ", end_c - start_c, end_t - start_t)
 
-        return Gj, Eavg / L, Evar / L / np.sqrt(num_sample)
+        return Gj, Eavg / L, Evar / (L**2) / num_sample
 
     def getLocal_no_OO(self, config):
         '''
@@ -804,7 +804,7 @@ class NQS_2d():
             Eavg = np.average(Earray)
             Evar = np.var(Earray)
             print(self.get_self_amp_batch()[:5])
-            print("E/N !!!!: ", Eavg / self.LxLy, "  Var: ", Evar / self.LxLy / np.sqrt(num_sample))
+            print("E/N !!!!: ", Eavg / self.LxLy, "  Var: ", Evar / (self.LxLy**2) / num_sample)
             Glist = self.NNet.vanilla_back_prop(configArray, Earray)
             # Reg
             for idx, W in enumerate(self.NNet.sess.run(self.NNet.para_list)):
@@ -814,7 +814,7 @@ class NQS_2d():
             end_c, end_t = time.clock(), time.time()
             print("monte carlo time ( backProp ): ", end_c - start_c, end_t - start_t)
             print("norm(G): ", np.linalg.norm(Gj))
-            return Gj, Eavg / self.LxLy, Evar / self.LxLy / np.sqrt(num_sample)
+            return Gj, Eavg / self.LxLy, Evar / (self.LxLy**2) / num_sample
         else:
             pass
 
@@ -850,7 +850,7 @@ class NQS_2d():
         Evar = np.var(Earray)
         # print(self.getSelfAmp())
         print(self.get_self_amp_batch()[:5])
-        print("E/N !!!!: ", Eavg / self.LxLy, "  Var: ", Evar / self.LxLy / np.sqrt(num_sample))
+        print("E/N !!!!: ", Eavg / self.LxLy, "  Var: ", Evar / (self.LxLy**2) / num_sample)
 
         #####################################
         #  Fj = 2<O_iH>-2<H><O_i>
@@ -906,7 +906,7 @@ class NQS_2d():
         end_c, end_t = time.clock(), time.time()
         print("Sij, Fj time: ", end_c - start_c, end_t - start_t)
 
-        return Gj, Eavg / self.LxLy, Evar / self.LxLy / np.sqrt(num_sample)
+        return Gj, Eavg / self.LxLy, Evar / (self.LxLy**2) / num_sample
 
     def local_E_2dAFH_batch(self, config_arr, J=1):
         '''

network/tf_wrapper.py
@@ -5,15 +5,19 @@ import numpy as np
 
 def select_optimizer(optimizer, learning_rate, momentum=0):
     if optimizer == 'Adam':
-        return tf.train.AdamOptimizer(learning_rate=learning_rate)
+        return tf.train.AdamOptimizer(learning_rate=learning_rate,
+                                      epsilon=1e-3) #previous 1e-8
     elif optimizer == 'Mom':
         return tf.train.MomentumOptimizer(learning_rate=learning_rate,
                                           momentum=momentum)
     elif optimizer == 'RMSprop':
         return tf.train.RMSPropOptimizer(learning_rate=learning_rate,
-                                         epsilon=0.1)
+                                         epsilon=1e-6) #previous 1e-1
     elif optimizer == 'GD':
         return tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+    elif optimizer == 'Adadelta':
+        return tf.train.AdadeltaOptimizer(learning_rate=learning_rate,
+                                          epsilon=1e-6)
     else:
         raise
 
@@ -28,8 +32,12 @@ def select_activation(activation):
         return tf.nn.relu
     elif activation == 'c_elu':
         return c_elu
+    elif activation == 'elu':
+        return elu
     elif activation == 'tanh':
         return tf.tanh
+    elif activation == 'selu':
+        return tf.nn.selu
 
 
 def leaky_relu(x):
@@ -190,8 +198,8 @@ def circular_conv_2d(bottom, filter_size, in_channels, out_channels,
     much faster than FFT approach generally, since the dimension of the filter is small.
     '''
     with tf.variable_scope(name, reuse=None):
-        filt, conv_biases = get_conv_var2d(filter_size, in_channels,
-                                           out_channels, biases=biases, bias_scale=bias_scale)
+        filt, conv_biases = get_conv_var2d(filter_size, in_channels, out_channels,
+                                           biases=biases, bias_scale=bias_scale)
         if not FFT:
             # bottom shape [None, Lx, Ly, channels]
             # pad_size = filter_size - 1
@@ -328,8 +336,9 @@ def get_conv_var2d(filter_size, in_channels, out_channels, name="",
 
     # initial_value = tf.truncated_normal([filter_size, filter_size, in_channels, out_channels], 0.0, 0.01)
     initial_value = tf.truncated_normal([filter_size, filter_size, in_channels, out_channels], 0.0,
-                                        np.sqrt(1. / (filter_size * filter_size * in_channels )))
+                                        np.sqrt(2. / (filter_size * filter_size * in_channels )))
                                         # np.sqrt(2. / (filter_size*filter_size*(in_channels+out_channels))))
+                                        # Xavier init
     filters = get_var(initial_value, name + "weights", dtype=dtype)
 
     if not biases:
@@ -349,7 +358,10 @@ def get_fc_var(in_size, out_size, name="", biases=True, dtype=tf.float32):
         pass
 
     # initial_value = tf.truncated_normal([in_size, out_size], 0.0, 0.1)
-    initial_value = tf.random_normal([in_size, out_size], stddev=np.sqrt(2./(in_size+out_size)))
+    # Xavier init
+    # initial_value = tf.random_normal([in_size, out_size], stddev=np.sqrt(2./(in_size+out_size)))
+    # He (MSAR) init
+    initial_value = tf.random_normal([in_size, out_size], stddev=np.sqrt(2./(in_size)))
     weights = get_var(initial_value, name + "weights", dtype=dtype)
 
     if biases: