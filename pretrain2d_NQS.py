ef433f21e2dcdff33da36c8542d0752c3ab10c68
ga63zuh <shenghsuan.lin@tum.de>
2021-02-09 17:54:54 +0100

    intermideate step in refactoring pretraining to supervised

--- pretrain2d_NQS.py
+++ pretrain2d_NQS.py
@@ -10,42 +10,142 @@ import network.tf_wrapper as tf_
 if __name__ == "__main__":
 
     args = parse_args()
-    L = args.L
-    which_net = args.which_net
-    lr = args.lr
-    batch_size = args.batch_size
-    J2 = args.J2
-    alpha = args.alpha
-    reg = args.reg
-    act = args.act
+    if bool(args.debug):
+        np.random.seed(0)
+        tf.set_random_seed(1234)
+    else:
+        pass
 
-    opt = args.opt  # "Mom"
-    system_size = (L, L, 2)
-    Net = tf_network(which_net, system_size, optimizer=opt, dim=2, alpha=alpha, activation=act)
+    (L, which_net, lr, num_sample) = (
+        args.L, args.which_net, args.lr, args.num_sample)
+    (J2, SR, reg, path) = (args.J2, bool(args.SR), args.reg, args.path)
+    (act, SP, using_complex) = (args.act, bool(args.SP), bool(args.using_complex))
+    (real_time, integration, pinv_rcond) = (
+        bool(args.real_time), args.integration, args.pinv_rcond)
 
-    X_half = np.genfromtxt('X2.csv', delimiter=',').reshape((60000, 8, 8))
-    X = np.zeros((60000,8,8,2))
-    X[:,:,:,0] = X_half
-    X[:,:,:,1] = 1-X_half
-    Y = np.genfromtxt('Y2.csv', delimiter=',').reshape((60000, 1))
-    # Y = np.sign(Y)
+    if len(path) > 0 and path[-1] != '/':
+        path = path + '/'
 
-    X_half = np.genfromtxt('X16.csv', delimiter=',')
-    X = np.zeros([2**16, 4, 4, 2])
-    X[:,:,:,0] = X_half.reshape([2**16, 4, 4])
-    X[:,:,:,1] = 1-X_half.reshape([2**16, 4, 4])
+    if args.alpha != 0:
+        alpha = args.alpha
+    else:
+        alpha = alpha_map[which_net]
+
+    opt, batch_size, H, dim, num_iter = (args.opt, args.batch_size,
+                                         args.H, args.dim, args.num_iter)
+    PBC = args.PBC
+
+    num_blocks, multi_gpus = args.num_blocks, args.multi_gpus
+    save_each = args.save_each
+    conserved_Sz, warm_up, Q_tar = bool(args.conserved_Sz), bool(args.warm_up), args.Q_tar
+    conserved_C4 = bool(args.conserved_C4)
+    conserved_inv = bool(args.conserved_inv)
+    conserved_SU2, chem_pot = bool(args.conserved_SU2), args.chem_pot
+    if not conserved_Sz:
+        assert Q_tar is None
+    else:
+        assert Q_tar is not None
+
+
+    if opt == "KFAC":
+        KFAC = True
+    else:
+        KFAC = False
+
+    if dim == 1:
+        systemSize = (L, 2)
+    elif dim == 2:
+        systemSize = (L, L, 2)
+        if H == 'Julian':
+            systemSize = (L, L, 3)
+        else:
+            pass
+
+    else:
+        raise NotImplementedError
+
+    Net = tf_network(which_net, systemSize, optimizer=opt, dim=dim, alpha=alpha,
+                     activation=act, using_complex=using_complex, single_precision=SP,
+                     batch_size=num_sample, num_blocks=num_blocks, multi_gpus=multi_gpus,
+                     conserved_C4=conserved_C4, conserved_Sz=conserved_Sz, Q_tar=Q_tar,
+                     conserved_SU2=conserved_SU2, chem_pot=chem_pot,
+                     conserved_inv=conserved_inv,
+                    )
+
+    # args = parse_args()
+    # L = args.L
+    # which_net = args.which_net
+    # lr = args.lr
+    # batch_size = args.batch_size
+    # J2 = args.J2
+    # alpha = args.alpha
+    # reg = args.reg
+    # act = args.act
+    # using_complex = args.using_complex
+    # opt = args.opt  # "Mom"
+    # conserved_Sz, warm_up, Q_tar = bool(args.conserved_Sz), bool(args.warm_up), args.Q_tar
+
+
+    # system_size = (L, L, 2)
+    # Net = tf_network(which_net, system_size, optimizer=opt,
+    #                  dim=2, alpha=alpha, activation=act,
+    #                  using_complex=using_complex,
+    #                  Q_tar=Q_tar
+    #                 )
+
+    X_computation_basis = np.genfromtxt('ExactDiag/basis_L%d.csv' % (L**2), delimiter=',')
+    X = np.zeros([2**(L**2), L, L, 2])
+    X[:,:,:,0] = X_computation_basis.reshape([2**(L**2), L, L])
+    X[:,:,:,1] = 1-X_computation_basis.reshape([2**(L**2), L, L])
     Y = np.genfromtxt('ExactDiag/EigVec/ES_2d_L4x4_J2_0.csv', delimiter=',')
+    Y = np.array(Y, dtype=np.complex128)[:, None]
+    pi = tf.constant(np.pi)
 
     print(X.shape, Y.shape)
 
     with Net.sess as sess:
 
-        true_out = tf.placeholder(tf.float32, [None, 1])
-        v1 = true_out
-        v2 = tf.real(Net.amp)
-        cost = -tf.reduce_sum(tf.multiply(v1, v2))/tf.norm(v1)/tf.norm(v2)
-        # cost = -tf.reduce_sum(tf.multiply(true_out, tf.log(Net.amp)))
-        # cost = tf.nn.l2_loss((Net.amp - true_out))
+        true_amp_log_re = tf.placeholder(tf.float32, [None, 1])
+        true_amp_log_im = tf.placeholder(tf.float32, [None, 1])
+        net_amp_log_re = tf.real(Net.log_amp)
+        net_amp_log_im = tf.imag(Net.log_amp)
+        assert net_amp_log_re.shape[1] == 1
+        ###################################
+        # Cost function : KL + L2 #
+        ###################################
+        kl_cost = tf.reduce_mean(2 * (true_amp_log_re - net_amp_log_re))
+        # l2_cost = tf.reduce_mean(tf.square( tf.mod(((true_amp_log_im - net_amp_log_im) + pi), 2*pi) - pi ))
+        # l2_cost = tf.reduce_mean(tf.square( (true_amp_log_im - tf.mod(net_amp_log_im, 2*pi) )))
+        l2_cost = tf.reduce_mean(tf.square(tf.cos(true_amp_log_im) - tf.cos(net_amp_log_im)) +
+                                 tf.square(tf.sin(true_amp_log_im) - tf.sin(net_amp_log_im)))
+        cost = kl_cost + l2_cost
+
+
+        true_amp = tf.placeholder(tf.complex64, [None, 1])
+        v1 = true_amp
+        v2 = Net.amp
+        # sampled_l2 = tf.reduce_mean(tf.square(tf.abs(v1 - v2)/tf.abs(v1)))
+        sampled_fidelity = tf.reduce_mean(tf.real( v2/v1 ))
+        # cost = -sampled_fidelity
+
+        ###################################
+        # Cost function 1: Batch fidelity #
+        ###################################
+        # cost = -tf.reduce_sum(tf.multiply(v1, v2))/tf.norm(v1)/tf.norm(v2)
+
+        ###################################
+        # KL-divergence + classficiation error
+        ###################################
+        # cost1 = - tf.log(tf.divide(v2**2+1e-60, v1**2 + 1e-60))
+        # cost2 = tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.sign(v1), logits=v2)
+        # cost = tf.reduce_sum(cost1 + cost2)
+
+        # cost = tf.reduce_sum( tf.multiply(tf.log(tf.divide(v2, v1)), tf.log(tf.divide(v1, v2))) )
+        # cost = -tf.real(tf.norm( tf.log(tf.complex(v2,0.))-tf.log(tf.complex(v1,0.)) ))
+        # cost = -tf.reduce_sum(tf.divide(v2,v1+1e-8)) + 1. * tf.norm(v2)
+
+        # cost = -tf.reduce_sum(tf.multiply(true_amp, tf.log(Net.amp)))
+        # cost = tf.nn.l2_loss((Net.amp - true_amp))
         for w in Net.para_list:
             cost += reg * tf.nn.l2_loss(w)
 
@@ -63,10 +163,11 @@ if __name__ == "__main__":
 
         print(len(Net.model_var_list), len(Net.para_list))
         saver = tf.train.Saver(Net.model_var_list)
-        # ckpt_path = 'wavefunction/Pretrain/'+which_net+'/L'+str(L)
-        ckpt_path = 'wavefunction/vmc2d/'+which_net+'_'+act+'/L'+str(L)+'a'+str(alpha)
+        ckpt_path = 'wavefunction/Pretrain/'+which_net+'/L'+str(L)
+        # ckpt_path = 'wavefunction/vmc2d/'+which_net+'_'+act+'/L'+str(L)+'a'+str(alpha)
         if not os.path.exists(ckpt_path):
             os.makedirs(ckpt_path)
+
         ckpt = tf.train.get_checkpoint_state(ckpt_path)
         if ckpt and ckpt.model_checkpoint_path:
             saver.restore(Net.sess, ckpt.model_checkpoint_path)
@@ -78,34 +179,51 @@ if __name__ == "__main__":
         print("-------- Start training -------\n")
         print(("Total num para: ", Net.getNumPara()))
 
-        p = (np.abs(Y)).flatten()
-        p = p/sum(p)
-        p = p + 100./batch_size
-        p = p/sum(p)
-        Y = Y/np.sum(np.abs(Y))
+        p = (np.abs(Y)**2).flatten()
+        # p = p + 100./batch_size
+        # p = p/sum(p)
 
         total_cos_accu = []
         batch_cos_accu = []
-        # import pdb;pdb.set_trace()
 
-        for i in range(100000):
-            # batch_mask = np.random.choice(len(Y), batch_size)  # ,p=Y*Y)
-            batch_mask = np.random.choice(len(Y), batch_size, p=p)
+        ##############################################################################
+        import sys
+        sys.path.append('/tuph/t30/space/ga63zuh/qTEBD/sampling_mps/')
+        sys.path.append('/tuph/t30/space/ga63zuh/qTEBD/')
+        import mps_func
+        import qTEBD
+        import sample_mps
+
+        state = np.genfromtxt('/home/t30/all/ga63zuh/Neural-Network-Quantum-State/ExactDiag/EigVec/ES_2d_L4x4_J2_0.csv', delimiter=',')
+        MPS = mps_func.state_2_MPS(state, 16, 1000)
+
+        MPS, err = qTEBD.right_canonicalize(MPS, no_trunc=True, chi=1000, normalized=True)
+        print("err = ", err)
+
+        #PIJ
+        sample_mps.ipj_trans_pij(MPS)
+        #IPJ
+
+        #################################################################################
+
+        for i in range(100000+1):
+            # batch_mask = np.random.choice(len(Y), batch_size, p=p)
+            # X_mini_batch = X[batch_mask]
+            # Y_mini_batch = Y[batch_mask]
+
+            configs, amps = sample_mps.parallel_sampling(MPS)
+            Y_mini_batch = np.array(amps, dtype=np.complex128)[:, None]
+            X_mini_batch = np.zeros([Y_mini_batch.size, L, L, 2])
+            X_mini_batch[:, :, :, 0] = configs.reshape([-1, L, L])
+            X_mini_batch[:, :, :, 1] = 1 - configs.reshape([-1, L, L])
+
+
+
 
             if i % 5000 == 0:
-                # y = sess.run(Net.amp, feed_dict={Net.x: X})
+                ### get full batch information
                 y = Net.get_amp(X)
                 print(('y norm : ', np.linalg.norm(y)))
-                y_list = []
-                for i in range(60):
-                    yi = Net.get_amp(X[i*1000:(i+1)*1000])
-                    y_list.append(yi)
-
-                y_list.append(Net.get_amp(X[60000:]))
-                y2 = np.concatenate(y_list)
-                print(('y2 norm : ', np.linalg.norm(y2)))
-                print(('diff in y-y2', np.linalg.norm(y-y2)))
-
                 c = Y.flatten().dot(y.flatten())/np.linalg.norm(Y)/np.linalg.norm(y)
                 print(c)
                 total_cos_accu.append(c)
@@ -118,20 +236,25 @@ if __name__ == "__main__":
                 if PLOT:
                     import matplotlib.pyplot as plt
                     fig = plt.figure()
-                    plt.plot(Y/np.linalg.norm(Y), '-o')
-                    plt.plot(y.real/np.linalg.norm(y.real), '--')
+                    plt.plot(Y[mask]/np.linalg.norm(Y[mask]), '-o')
+                    plt.plot(y[mask].real/np.linalg.norm(y[mask].real), '--')
                     plt.show()
                 pass
 
-            _, c, y = sess.run([train_step, cost, Net.amp], feed_dict={Net.x: X[batch_mask],
-                                                                       true_out: Y[batch_mask]})
+            _, c, c1, c2, f = sess.run([train_step, cost, kl_cost, l2_cost, sampled_fidelity],
+                                       feed_dict={Net.x: X_mini_batch,
+                                                  true_amp_log_re: np.real(np.log(Y_mini_batch)),
+                                                  true_amp_log_im: np.imag(np.log(Y_mini_batch)),
+                                                  true_amp: Y_mini_batch})
             batch_cos_accu.append(-c)
+            if i % 50 == 0:
+                print("iter=", i, "cost=", c, "kl_cost=", c1, "l2_cost=", c2, "F=", f)
 
             if i % 500 == 0:
                 print(("step:", i, " cosine accuracy:", -c, "Y norm",
-                      np.linalg.norm(Y[batch_mask]), "y norm:", np.linalg.norm(y)))
-# "dot:",
-# y.T.dot(Y[batch_mask])[0]/np.linalg.norm(Y[batch_mask])/np.linalg.norm(y))
+                       np.linalg.norm(Y_mini_batch), "y norm:", np.linalg.norm(y)))
+                # "dot:",
+                # y.T.dot(Y_mini_batch)[0]/np.linalg.norm(Y_mini_batch)/np.linalg.norm(y))
                 saver.save(sess, ckpt_path + '/pre')
 
     np.savetxt('log/pretrain/L%d_%s_a%s_%s%.e_batch.csv' % (L, which_net, alpha, opt, lr),
--- pretrain_NQS.py
+++ pretrain_NQS.py
@@ -16,78 +16,51 @@ if __name__ == "__main__":
     batch_size = args.batch_size
     J2 = args.J2
     alpha = args.alpha
+    reg = args.reg
+    act = args.act
     using_complex = args.using_complex
-
     opt = args.opt  # "Mom"
+
     system_size = (L, 2)
-    Net = tf_network(which_net, system_size, optimizer=opt, dim=1, alpha=alpha,
-                     using_complex=using_complex)
+    Net = tf_network(which_net, system_size, optimizer=opt,
+                     dim=1, alpha=alpha, activation=act,
+                     using_complex=using_complex
+                    )
 
-    Net.run_global_variables_initializer()
-
-    basis = []
-    for line in open('ExactDiag/Sz0_basisMatrix'+str(L)+'.csv', 'r'):
-        basis.append(line[:-1])
-
-    newbasis = np.zeros((len(basis), L, 2))
-    for i in range(len(basis)):
-        for j in range(L):
-            newbasis[i, j, 0] = basis[i][j]
-
-    num_train = 2**L
-    to_large_dict = {}
-    X = np.zeros((num_train, L, 2))
-    for i in range(0, num_train):
-        temp = i+1
-        for j in range(L):
-            if temp > 2**(L-j-1):
-                X[i, j, 0] = 1
-                temp -= 2**(L-j-1)
-            else:
-                X[i, j, 1] = 1
-                pass
-        to_large_dict[''.join([str(int(ele)) for ele in X[i, :, 0]])] = i
-
-    # X = X.reshape((num_train, L*2))
-
-    # Y = np.zeros((num_train, 1))
-    # amp_array = np.genfromtxt(open('EigenVec/EigVec_L'+str(L)+'V20W0E0.csv', 'r'))
-    # for i in range(len(amp_array)):
-        #     idx = to_large_dict[''.join([str(int(ele)) for ele in newbasis[i, :, 0]])]
-        #     Y[idx] = amp_array[i]
-
-    # X_nz = X[(np.abs(Y) * np.ones((1, 32)) >= 1e-15)]
-    # Y_nz = Y[(np.abs(Y) >= 1e-15)]
-    # X = X_nz.reshape((len(Y_nz), 32))
-    # Y = Y_nz.reshape((len(Y_nz),1))
-
-    # y for all Sz sector #
+    X_computation_basis = np.genfromtxt('ExactDiag/basis_L%d.csv' % L, delimiter=',')
+    X = np.zeros([2**L, L, 2])
+    X[:,:,0] = X_computation_basis.reshape([2**L, L])
+    X[:,:,1] = 1-X_computation_basis.reshape([2**L, L])
     Y = np.genfromtxt('ExactDiag/EigVec/ES_L'+str(L)+'_J2_'+str(int(J2*10))+'_OBC.csv').reshape((2**L, 1))
-    # Y = np.sign(Y)
+
     print(X.shape, Y.shape)
-    # Y = Y * np.sqrt(Y.size)
-    import pdb;pdb.set_trace()
 
     with Net.sess as sess:
 
-        true_out = tf.placeholder(tf.float32, [None, 1])
-        v1 = true_out
+        true_amp = tf.placeholder(tf.complex64, [None, 1])
+        v1 = true_amp
         v2 = Net.amp
-        # Batch fidelity
-        # cost = -tf.reduce_sum(tf.multiply(v1, v2))/tf.norm(v1)/tf.norm(v2)
+        ###################################
+        # Cost function 1: Batch fidelity #
+        ###################################
+        cost = -tf.reduce_sum(tf.multiply(v1, v2))/tf.norm(v1)/tf.norm(v2)
 
+        ###################################
         # KL-divergence + classficiation error
-        cost1 = - tf.log(tf.divide(v2**2+1e-60, v1**2 + 1e-60))
-        cost2 = tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.sign(v1), logits=v2)
-        cost = tf.reduce_sum(cost1 + cost2)
-
+        ###################################
+        # cost1 = - tf.log(tf.divide(v2**2+1e-60, v1**2 + 1e-60))
+        # cost2 = tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.sign(v1), logits=v2)
+        # cost = tf.reduce_sum(cost1 + cost2)
 
         # cost = tf.reduce_sum( tf.multiply(tf.log(tf.divide(v2, v1)), tf.log(tf.divide(v1, v2))) )
         # cost = -tf.real(tf.norm( tf.log(tf.complex(v2,0.))-tf.log(tf.complex(v1,0.)) ))
         # cost = -tf.reduce_sum(tf.divide(v2,v1+1e-8)) + 1. * tf.norm(v2)
 
-        # cost = -tf.reduce_sum(tf.multiply(true_out, tf.log(Net.amp)))
-        # cost = tf.nn.l2_loss((Net.amp - true_out))
+        # cost = -tf.reduce_sum(tf.multiply(true_amp, tf.log(Net.amp)))
+        # cost = tf.nn.l2_loss((Net.amp - true_amp))
+        for w in Net.para_list:
+            cost += reg * tf.nn.l2_loss(w)
+
 
         learning_rate = tf.Variable(lr)
         Optimizer = tf_.select_optimizer(optimizer=opt, learning_rate=learning_rate,
@@ -117,20 +90,19 @@ if __name__ == "__main__":
         print("-------- Start training -------\n")
         print(("Total num para: ", Net.getNumPara()))
 
-        p = (np.abs(Y**2)).flatten()
+        p = (np.abs(Y)**2).flatten()
         # p = p + 100./batch_size
         # p = p/sum(p)
 
         total_cos_accu = []
         batch_cos_accu = []
-        # import pdb;pdb.set_trace()
 
         for i in range(100000+1):
-            # batch_mask = np.random.choice(len(Y), batch_size)  # ,p=Y*Y)
             batch_mask = np.random.choice(len(Y), batch_size, p=p)
 
             if i % 5000 == 0:
-                y = sess.run(Net.amp, feed_dict={Net.x: X})
+                ### get full batch information
+                y = Net.get_amp(X)
                 print(('y norm : ', np.linalg.norm(y)))
                 c = Y.flatten().dot(y.flatten())/np.linalg.norm(Y)/np.linalg.norm(y)
                 print(c)
@@ -144,7 +116,7 @@ if __name__ == "__main__":
                     plt.show()
 
             _, c, y = sess.run([train_step, cost, Net.amp], feed_dict={Net.x: X[batch_mask],
-                                                                       true_out: Y[batch_mask]})
+                                                                       true_amp: Y[batch_mask]})
             batch_cos_accu.append(-c)
 
             if i % 500 == 0:
